515728f7911a6e77670e26ac687a4b8b3b489a5d
ismahaneyy <liciousmayrah@gmail.com>
2022-11-24 10:16:42 +0300

    Finished assignment

---
+++ index.js
@@ -0,0 +1,19 @@
+//function expression called divide
+const divide = (parameter1, parameter2) => {
+    return 20
+}
+divide(2000, 100);
+
+
+//arrow function called square
+const square = x => {
+     return x * x
+}
+square(4);
+
+
+// arrow function called add
+const add = (a, b) => {
+   return a + b ;
+}
+add(3, 4);
